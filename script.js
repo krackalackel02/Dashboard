9ae0e8ade556f7b405096b2deec5596dc37112c1
Paul Kallarackel <108292404+krackalackel02@users.noreply.github.com>
2023-07-10 22:08:23 +0100

    initial disapper of nav

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ window.onload = () => {
     const root = document.documentElement;
     const rotAnimDur = getComputedStyle(root).getPropertyValue("--rotate-animation-duration");
     const dashButton = document.querySelector(".nav-title .material-symbols-outlined");
-  
+    const navLinks = document.getElementById("nav-links");
     dashButton.addEventListener("click", (e) => {
       e.target.classList.add("rotate");
+      navLinks.classList.toggle("collapsed");
       setTimeout(() => {
         e.target.classList.remove("rotate");
       }, parseFloat(rotAnimDur)); // Use the parsed duration value in milliseconds
