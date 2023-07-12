ff33819c5a00cd3719f67b9f66ea36faf206b1e1
Paul Kallarackel <108292404+krackalackel02@users.noreply.github.com>
2023-07-12 20:25:49 +0100

    progromatic project creation and card css

--- projects.js
+++ projects.js
@@ -1,37 +1,37 @@
 const myProjects = [
     {
         name: 'Custom Form',
-        img: 'profile pic.webp',
+        img: 'custom form.png',
         link: 'https://krackalackel02.github.io/Custom-Form',
         details: 'details'
     },
     {
         name: 'Matching Game',
-        img: 'profile pic.webp',
+        img: 'matching game.png',
         link: 'https://krackalackel02.github.io/Matching-Game',
         details: 'details'
     },
     {
         name: 'Calculator',
-        img: 'profile pic.webp',
+        img: 'calculator.png',
         link: 'https://krackalackel02.github.io/Calculator',
         details: 'details'
     },
     {
         name: 'Random Colour',
-        img: 'profile pic.webp',
+        img: 'colour changer.png',
         link: 'https://krackalackel02.github.io/Random-Colour',
         details: 'details'
     },
     {
         name: 'Recipes',
-        img: 'profile pic.webp',
+        img: 'recipes.png',
         link: 'https://krackalackel02.github.io/Recipes',
         details: 'details'
     },
     {
         name: 'Counter',
-        img: 'profile pic.webp',
+        img: 'counter.png',
         link: 'https://krackalackel02.github.io/Counter',
         details: 'details'
     }
